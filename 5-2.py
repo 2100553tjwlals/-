d539b3db7bf5c6e78b9ced9912c458261bcee8a3
tjwlals <tjwl0418@naver.com>
2021-05-11 21:47:19 +0900

    5월 11일

---
+++ 5-2.py
@@ -0,0 +1,14 @@
+x = 3
+
+if x > 2:    #만약 x가 2보다 크면 출력됨
+    print("hello")
+    
+    
+if x > 5:
+    print("hello")
+elif x == 3: # elif-> else if -> 아니면 만약 (조건이 있다)
+    print("bye")
+else:     # 그게아니라면 출력됨 (특정 조건이 없다)
+    print("hi")
+
+# 같다 표현은 =가 아닌 ==이다
---
+++ 5-3.py
@@ -0,0 +1,5 @@
+if not x > y:# x가 y보다 크지않으면
+    
+if x > z and x > y: # 두 조건이 모두맞아야 한다
+    
+if x > Z or x > y: # 두 조건중 하나만 맞으면 된다
