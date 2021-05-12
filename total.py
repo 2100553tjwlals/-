3bb9350646d52ce6bd3a509508e96dadd5faa3c9
tjwlals <tjwl0418@naver.com>
2021-05-12 22:27:51 +0900

    2021-05-12

---
+++ total.py
@@ -0,0 +1,12 @@
+def  sayHello ( name , age ) :
+    if  age  <  10 :
+        print ( "안녕,"  +  name )
+    elif  age  <= 20  and  age  >=  10 :
+        print ( "안녕하세요,"  +  name )    
+    else :
+        print ( "안녕하십니까,"  +  name )
+ 
+sayHello ( "태희" , 20 )
+sayHello ( "정수" , 6 )
+sayHello ( "세영" , 40 )
+sayHello ( "철수" , 10 )
