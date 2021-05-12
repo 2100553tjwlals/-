2fe1c962339333ef6e324d68f4f06c685e631eb1
tjwlals <tjwl0418@naver.com>
2021-05-12 22:23:55 +0900

    2021-05-12

---
+++ 6.py
@@ -0,0 +1,7 @@
+def chat(name1, name2, age):
+    print("%s : 넌 몇살이니?" %name1) # %s는 name1을 받는다 
+    print("%s : 나? 나는 %d" %(name2, age) + "살이야") # %d는 age를 받는다
+
+chat("정수","태희",20)
+chat("환준", "세영", 19)
+
