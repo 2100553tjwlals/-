cbc563322b786f6cb961e29a010237734dcddf0d
tjwlals <tjwl0418@naver.com>
2021-05-16 23:58:28 +0900

    2021-05-16

---
+++ 10-2list.py
@@ -0,0 +1,9 @@
+x = [1,2,3,4]
+y = ["Hello", "World"]
+z = ["Bye", 1,2,3]
+
+print(x)
+print(y)
+print(z)
+print(x+y)
+print(x+y+z)
---
+++ 10-4list.py
@@ -0,0 +1,5 @@
+x = [4, 2, 1, 3]
+y = ["라","가","다","나"]
+
+xsort = sorted(x) # sorted 정렬
+ysort = sorted(y)
